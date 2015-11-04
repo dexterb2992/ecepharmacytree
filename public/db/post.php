9adc7a9f16ee479b8a6e6c0302a33d66276bdc50
Zem <lourdrivera123@gmail.com>
2015-11-04 14:50:58 +0800

    asdas

--- public/db/post.php
+++ public/db/post.php
@@ -288,7 +288,7 @@ if ($request == 'register') {
     	}
     } else if ($action == 'check_and_delete_consultation') {
     	if($_POST['consultation_request'] == "delete"){
-    		$sql = "DELETE FROM ".$_POST['table']." WHERE is_approved = 0 and id = ".$_POST['id'];
+    		$sql = "DELETE FROM ".$_POST['table']." WHERE (is_approved = 0 or is_approved = 2) and id = ".$_POST['id'];
     	} else {
     		$settings = "";
     		foreach ($_POST as $key => $value) {
@@ -328,7 +328,7 @@ if ($request == 'register') {
     	if(mysql_query($sql)) {
     		if(mysql_affected_rows() > 0)
     			$response["success"] = 1;
-    			$response['server_id'] = 
+    			$response['server_id'] = mysql_affected_rows()
     		else {
     			$response["success"] = 0;
     			$response["message"] = "No record is updated or deleted";
