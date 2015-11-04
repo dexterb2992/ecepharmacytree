dc1f97b1525db538ef79fdd317e8b03a73a12e84
Zem <lourdrivera123@gmail.com>
2015-11-04 16:46:10 +0800

    sada;

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,17 +14,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
-
+        $this->call(Illuminate\Database\Seeder\RegionTableSeeder::class);
+        $this->call(Illuminate\Database\Seeder\ProvinceTableSeeder::class);
+        $this->call(Illuminate\Database\Seeder\MunicipalityTableSeeder::class);
+        $this->call(Illuminate\Database\Seeder\BarangayTableSeeder::class);
         $this->call(Illuminate\Database\Seeder\BranchTableSeeder::class);
         $this->call(Illuminate\Database\Seeder\UserTableSeeder::class);
         $this->call(Illuminate\Database\Seeder\ProductCategoryTableSeeder::class);
         $this->call(Illuminate\Database\Seeder\ProductSubcategoryTableSeeder::class);
         $this->call(Illuminate\Database\Seeder\ProductTableSeeder::class);
         $this->call(Illuminate\Database\Seeder\SettingTableSeeder::class);
-        $this->call(Illuminate\Database\Seeder\RegionTableSeeder::class);
-        $this->call(Illuminate\Database\Seeder\ProvinceTableSeeder::class);
-        $this->call(Illuminate\Database\Seeder\MunicipalityTableSeeder::class);
-        $this->call(Illuminate\Database\Seeder\BarangayTableSeeder::class);
         $this->call(Illuminate\Database\Seeder\PatientTableSeeder::class);
         // Model::reguard();
     }
