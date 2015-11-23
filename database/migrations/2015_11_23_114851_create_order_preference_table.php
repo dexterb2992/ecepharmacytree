ebafe1838dc50d92c66b92e3ba116c1384a9688d
Zem <lourdrivera123@gmail.com>
2015-11-23 16:27:49 +0800

    sads

---
+++ database/migrations/2015_11_23_114851_create_order_preference_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderPreferenceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_preference', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('patient_id')->unsigned();
+            $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
+            $table->string('order_receiving_option');
+            $table->string('address_for_delivery');
+            $table->string('recipient_for_delivery');
+            $table->string('payment_method');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('patient_records');
+    }
+}
