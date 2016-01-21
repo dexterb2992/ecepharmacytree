42727bb21dce0c83adf33a982fb42079a3bfebdc
Zem <lourdrivera123@gmail.com>
2016-01-21 22:20:58 +0800

    asda

--- resources/views/admin/order.blade.php
+++ resources/views/admin/order.blade.php
@@ -155,6 +155,13 @@
                   @if($order_detail->available_quantity == 0)
                   <i>Out of Stock</i><br/>
                   <b>Items to be fulfilled ( {{ $order_detail->quantity }} )</b>
+                  @elseif($order_detail->quantity > $order_detail->available_quantity)
+                  <div class="input-group">
+                    <input type="number" name="order_fulfillment_qty[{{ $order_detail->id }}]" class="form-control" value="{{ $order_detail->available_quantity }}" max="{{ $order_detail->available_quantity }}" min="0">
+                    <div class="input-group-addon">
+                      of  <strong>{{ $order_detail->quantity - $order_detail->qty_fulfilled }}</strong>
+                    </div>
+                  </div>
                   @else
                   <div class="input-group">
                     <input type="number" name="order_fulfillment_qty[{{ $order_detail->id }}]" class="form-control" value="{{ $order_detail->quantity - $order_detail->qty_fulfilled }}" max="{{ $order_detail->quantity - $order_detail->qty_fulfilled }}" min="0">
