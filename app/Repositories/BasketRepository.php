8014262ce135376f2c640135410d52aa8aa49e2d
Zem <lourdrivera123@gmail.com>
2016-01-23 03:11:09 +0800

    trial and error

--- app/Repositories/BasketRepository.php
+++ app/Repositories/BasketRepository.php
@@ -11,6 +11,7 @@ class BasketRepository {
 	function check_and_adjust_basket($patient_id, $branch_id){
 
 		$response = array();
+		$final_array = array();
 		$basket_quantity_changed = false;
 		$basket_promo_removed = false;
 
@@ -19,20 +20,29 @@ class BasketRepository {
 		foreach($results as $result){
 			if($result->quantity > $result->available_quantity) {
 				$basket = Basket::findOrFail($result->basket_id);
-				$basket->quantity = $result->available_quantity;
-				if($basket->save()){
-					$result->quantity = $result->available_quantity;     
-					$basket_quantity_changed = true;               
-					$basketpromo = BasketPromo::where('basket_id', $basket->id)->first();
+				if($result->available_quantity == 0){
+					if($basket->delete())
+						$basket_quantity_changed = true;
+				} else {
+					$basket->quantity = $result->available_quantity;
+					if($basket->save()){
+						$result->quantity = $result->available_quantity;     
+						$basket_quantity_changed = true;  
+						array_push($final_array, $result);
+					}
+				}
+				$basketpromo = BasketPromo::where('basket_id', $basket->id)->first();
+				if(!empty($basketpromo)){
 					$response['basket_promo'] = $basketpromo;
 					$response['basket_id'] = $basket->id;
 					if($basketpromo->delete())
-						$basket_promo_removed = true;
-				}
+						$basket_promo_removed = true;	
+				}				
+
 			}
 
 		}
-		$response['baskets'] = $results;
+		$response['baskets'] = $final_array;
 		$response['success'] = 1;
 		$response['server_timestamp'] = date("Y-m-d H:i:s", time());
 		$response['basket_quantity_changed'] = $basket_quantity_changed;
