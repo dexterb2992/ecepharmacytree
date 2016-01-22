c6e77561633a107431163749c876a013fb14b6e6
Zem <lourdrivera123@gmail.com>
2016-01-22 11:05:00 +0800

    asdas

---
+++ database/migrations/2016_01_22_095632_create_basket_promos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBasketPromosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('basket_promos', function(Blueprint $table) {
+            $table->increments('id');        
+            $table->integer('basket_id')->unsigned();
+            $table->foreign('basket_id')->references('id')->on('patients')->onDelete('cascade');
+            $table->integer('promo_id')->unsigned();
+            $table->foreign('promo_id')->references('id')->on('patients')->onDelete('cascade');
+            $table->string('promo_type');
+            $table->double('percentage_discount');
+            $table->double('peso_discount');
+            $table->integer('free_gift_item');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
