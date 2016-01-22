b78e1892c4c128e842a687649040f2378bddb58b
Esel <rosellbarnes95@gmail.com>
2016-01-22 17:47:58 +0800

    aadas

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -40,7 +40,12 @@ class ApiController extends Controller
     $patient_id = $input['patient_id'];
 
     $result = DB::select("SELECT cpd.*, cpr.created_at as cpr_created_at, cpr.*, ct.*, cpr.id as cpr_id from clinic_patient_doctor as cpd inner join clinic_patients_records as cpr on cpd.clinic_patients_id = cpr.patient_id LEFT JOIN clinic_treatments as ct on cpr.id = ct.clinic_patients_record_id where BINARY cpd.username = '".$username."' and BINARY cpd.password = '".$password."' and ( cpd.patient_id = 0 or cpd.patient_id = ".$patient_id.")");
-
+    
+    if(!empty($result)){
+    	return $result;
+    } else {
+    	return 'way sulod';
+    }
     // if () {
     // 	// $response['records']
     //     $row_cp = mysql_fetch_object($result);
