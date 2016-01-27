0df605dbb2075f36d33a65394a94794ea730524d
Zem <lourdrivera123@gmail.com>
2016-01-27 13:46:18 +0800

    potang ina

--- database/seeds/ClinicPatientDoctorTableSeeder.php
+++ database/seeds/ClinicPatientDoctorTableSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 
 use ECEPharmacyTree\ClinicPatientDoctor;
-class ClinicMedicineTableSeeder extends Seeder
+class ClinicPatientDoctorTableSeeder extends Seeder
 {
 	public function run()
 	{
