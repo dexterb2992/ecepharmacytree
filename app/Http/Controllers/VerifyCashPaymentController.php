9d77b3096f4accd0eb9707cacc3cafc7bdd20727
Esel <rosellbarnes95@gmail.com>
2016-02-03 17:18:28 +0800

    asdas

--- app/Http/Controllers/VerifyCashPaymentController.php
+++ app/Http/Controllers/VerifyCashPaymentController.php
@@ -177,7 +177,7 @@ class VerifyCashPaymentController extends Controller
 				$order_details = DB::select("SELECT od.id, p.name as product_name, od.price, od.quantity, o.created_at as ordered_on, o.status,  p.packing, p.qty_per_packing, p.unit from order_details as od inner join orders as o on od.order_id = o.id inner join products as p on od.product_id = p.id inner join branches as br on o.branch_id = br.id where od.order_id =  ".$order_id." order by od.created_at DESC");
 				$order_date = Carbon::parse($order_date)->toFormattedDateString();
 				$order_time = Carbon::parse($order_date)->format('h:i A');
-				$this->emailtestingservice($email, $order_details, $recipient_name, $recipient_address, $recipient_contactNumber, $payment_method, $modeOfDelivery, $coupon_discount, $points_discount, $totalAmount_final, $gross_total, $order_id, $order_details, $order_date, $status);				
+				$this->emailtestingservice($email, $order_details, $recipient_name, $recipient_address, $recipient_contactNumber, $payment_method, $modeOfDelivery, $coupon_discount, $points_discount, $totalAmount_final, $gross_total, $order_id, $order_details, $order_date, $status, $order_time);				
 			}
 		}
 
