da6a676deab04e7d15701dfcbfe5f307ba5b4f36
Zem <lourdrivera123@gmail.com>
2016-02-03 14:54:45 +0800

    asda

--- resources/views/emails/pharmacy_tree_invoice.blade.php
+++ resources/views/emails/pharmacy_tree_invoice.blade.php
@@ -1,97 +1,148 @@
 <!DOCTYPE html>
-<html style="box-sizing: border-box;">
-  <head style="box-sizing: border-box;">
-    <meta charset="UTF-8" style="box-sizing: border-box;">
-    <title style="box-sizing: border-box;"> Pharmacy Tree | Invoice</title>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>AdminLTE 2 | Invoice</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.4 -->
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <!-- Font Awesome Icons -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Ionicons -->
+    <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
+    <!-- Theme style -->
+    <link href="../../dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
   </head>
-  <body style="box-sizing: border-box;font-family: &quot;Helvetica Neue&quot;,Helvetica,Arial,sans-serif;font-weight: 400;font-size: 14px;line-height: 1.42857;color: #333;">
-    <div class="wrapper" style="box-sizing: border-box;">
+  <body onload="window.print();">
+    <div class="wrapper">
       <!-- Main content -->
-      <section class="invoice" style="box-sizing: border-box;display: block;position: relative;background: #FFF none repeat scroll 0% 0%;border: 1px solid #F4F4F4;padding: 20px;margin: 10px 25px;">
+      <section class="invoice">
         <!-- title row -->
-        <div class="row" style="box-sizing: border-box;">
-          <div class="col-xs-12" style="box-sizing: border-box;width: 100%;float: left;position: relative;min-height: 1px;padding-right: 15px;padding-left: 15px;">
-            <h2 class="page-header" style="box-sizing: border-box;margin: 40px 0px 20px;font-size: 22px;padding-bottom: 9px;border-bottom: 1px solid #EEE;">
-              <i class="fa fa-globe" style="box-sizing: border-box;"></i> Pharmacy Tree
-              <small class="pull-right" style="box-sizing: border-box;font-size: 65%;color: #666;display: block;margin-top: 5px;float: right !important;">Date: {{ $order_date }}</small>
+        <div class="row">
+          <div class="col-xs-12">
+            <h2 class="page-header">
+              <i class="fa fa-globe" style="display: inline-block;font: normal normal normal 14px/1 FontAwesome;font-size: inherit;text-rendering: auto;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;transform: translate(0, 0);"></i> AdminLTE, Inc.
+              <small class="pull-right" style="float: right;">Date: 2/10/2014</small>
             </h2>
           </div><!-- /.col -->
         </div>
         <!-- info row -->
-        <div class="row invoice-info" style="box-sizing: border-box;">
-          <div class="col-sm-4 invoice-col" style="box-sizing: border-box;width: 33.3333%;float: left;position: relative;min-height: 1px;padding-right: 15px;padding-left: 15px;">
+        <div class="row invoice-info">
+          <div class="col-sm-4 invoice-col">
             From
-            <address style="box-sizing: border-box;">
-              <strong style="box-sizing: border-box;">Admin, Inc.</strong><br style="box-sizing: border-box;">
-              795 Folsom Ave, Suite 600<br style="box-sizing: border-box;">
-              San Francisco, CA 94107<br style="box-sizing: border-box;">
-              Phone: (804) 123-5432<br style="box-sizing: border-box;">
+            <address>
+              <strong>Admin, Inc.</strong><br>
+              795 Folsom Ave, Suite 600<br>
+              San Francisco, CA 94107<br>
+              Phone: (804) 123-5432<br>
+              Email: info@almasaeedstudio.com
             </address>
           </div><!-- /.col -->
-          <div class="col-sm-4 invoice-col" style="box-sizing: border-box;width: 33.3333%;float: left;position: relative;min-height: 1px;padding-right: 15px;padding-left: 15px;">
+          <div class="col-sm-4 invoice-col">
             To
-            <address style="box-sizing: border-box;">
-              <strong style="box-sizing: border-box;">{{ $recipient_name }}</strong><br style="box-sizing: border-box;">
-              {{ $recipient_address }}<br style="box-sizing: border-box;">
-              Phone: {{ $recipient_contactNumber }}<br style="box-sizing: border-box;">
-              Email: {{ $email }}
+            <address>
+              <strong>John Doe</strong><br>
+              795 Folsom Ave, Suite 600<br>
+              San Francisco, CA 94107<br>
+              Phone: (555) 539-1037<br>
+              Email: john.doe@example.com
             </address>
           </div><!-- /.col -->
-          <div class="col-sm-4 invoice-col" style="box-sizing: border-box;width: 33.3333%;float: left;position: relative;min-height: 1px;padding-right: 15px;padding-left: 15px;">
-            <!-- <b style="box-sizing: border-box;">Order ID:</b> {{ $order_id }}<br style="box-sizing: border-box;"> -->
-            <!-- <b>Payment Due:</b> 2/22/2014<br/> -->
-            <!-- <b>Account:</b> 968-34567 -->
+          <div class="col-sm-4 invoice-col">
+            <b>Invoice #007612</b><br>
+            <br>
+            <b>Order ID:</b> 4F3S8J<br>
+            <b>Payment Due:</b> 2/22/2014<br>
+            <b>Account:</b> 968-34567
           </div><!-- /.col -->
         </div><!-- /.row -->
 
         <!-- Table row -->
-        <div class="row" style="box-sizing: border-box;margin-top:20px !important;">
-          <div class="col-xs-12 table-responsive" style="box-sizing: border-box;width: 100%;min-height: .01%;padding-right: 15px;padding-left: 15px;overflow-x: auto;">
-            <table class="table table-striped" style="box-sizing: border-box;border-spacing: 0;border-collapse: collapse;background-color: transparent;width: 100%;max-width: 100%;margin-bottom: 0;">
-              <thead style="box-sizing: border-box;">
-                <tr style="box-sizing: border-box;">
-                  <th style="box-sizing: border-box;padding: 8px;text-align: left;line-height: 1.42857143;vertical-align: bottom;border-top: 1px solid #ddd;border-bottom: 2px solid #ddd;white-space: nowrap;">Product</th>
-                  <th style="box-sizing: border-box;padding: 8px;text-align: left;line-height: 1.42857143;vertical-align: bottom;border-top: 1px solid #ddd;border-bottom: 2px solid #ddd;white-space: nowrap;">Price x Qty</th>
-                  <th style="box-sizing: border-box;padding: 8px;text-align: left;line-height: 1.42857143;vertical-align: bottom;border-top: 1px solid #ddd;border-bottom: 2px solid #ddd;white-space: nowrap;">Subtotal</th>
+        <div class="row">
+          <div class="col-xs-12 table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>Qty</th>
+                  <th>Product</th>
+                  <th>Serial #</th>
+                  <th>Description</th>
+                  <th>Subtotal</th>
                 </tr>
               </thead>
-              <tbody style="box-sizing: border-box;">
-                @foreach($order_details as $order_detail)
-                <tr style="box-sizing: border-box;">
-                  <td style="box-sizing: border-box;padding: 8px;line-height: 1.42857143;vertical-align: top;border-top: 1px solid #ddd;white-space: nowrap;">{{ $order_detail->product_name }} </td>
-                  <td style="box-sizing: border-box;padding: 8px;line-height: 1.42857143;vertical-align: top;border-top: 1px solid #ddd;white-space: nowrap;">₱ {{ $order_detail->price.' X '.$order_detail->quantity }}</td>
-                  <td style="box-sizing: border-box;padding: 8px;line-height: 1.42857143;vertical-align: top;border-top: 1px solid #ddd;white-space: nowrap;">₱ {{ $order_detail->price * $order_detail->quantity }}</td>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Call of Duty</td>
+                  <td>455-981-221</td>
+                  <td>El snort testosterone trophy driving gloves handsome</td>
+                  <td>$64.50</td>
                 </tr>
-                @endforeach
+                <tr>
+                  <td>1</td>
+                  <td>Need for Speed IV</td>
+                  <td>247-925-726</td>
+                  <td>Wes Anderson umami biodiesel</td>
+                  <td>$50.00</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Monsters DVD</td>
+                  <td>735-845-642</td>
+                  <td>Terry Richardson helvetica tousled street art master</td>
+                  <td>$10.70</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Grown Ups Blue Ray</td>
+                  <td>422-568-642</td>
+                  <td>Tousled lomo letterpress</td>
+                  <td>$25.99</td>
+                </tr>
               </tbody>
             </table>
           </div><!-- /.col -->
         </div><!-- /.row -->
 
-        <div class="row" style="box-sizing: border-box;margin-top:20px !important;">
+        <div class="row">
           <!-- accepted payments column -->
-          <div class="col-xs-6" style="box-sizing: border-box;min-height: 1px;padding-right: 15px;padding-left: 15px;width: 50% !important;">
-            <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;box-sizing: border-box;margin: 0px 0px 10px;color: #777;padding: 19px;border-radius: 4px;min-height: 20px;margin-bottom: 20px;background-color: #F5F5F5;border: 1px solid #E3E3E3;box-shadow: none !important;">
-              <strong> Order ID: {{ $order_id }} </strong><br style="box-sizing: border-box;">
-              Order receiving option: {{ $modeOfDelivery }}<br style="box-sizing: border-box;">
-              Payment method: {{ $payment_method }}
+          <div class="col-xs-6">
+            <p class="lead">Payment Methods:</p>
+            <img src="../../dist/img/credit/visa.png" alt="Visa">
+            <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
+            <img src="../../dist/img/credit/american-express.png" alt="American Express">
+            <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
+            <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
+              Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem plugg dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
             </p>
           </div><!-- /.col -->
-          <div class="col-xs-6" style="box-sizing: border-box;padding-right: 15px;padding-left: 15px;width: 50% !important;">
-            <!-- <p class="lead" style="box-sizing: border-box;margin: 0px 0px 10px;font-size: 21px;">Amount Due 2/22/2014</p> -->
-            <div class="table-responsive" style="box-sizing: border-box;min-height: .01%;overflow-x: auto;">
-              <table class="table" style="box-sizing: border-box;border-spacing: 0;border-collapse: collapse;background-color: transparent;width: 100%;max-width: 100%;margin-bottom: 0;">
-                <tr style="box-sizing: border-box;">
-                  <th style="width: 50%;box-sizing: border-box;padding: 0;text-align: left;">Subtotal:</th>
-                  <td style="box-sizing: border-box;padding: 0;">{{ $gross_total }}</td>
+          <div class="col-xs-6">
+            <p class="lead">Amount Due 2/22/2014</p>
+            <div class="table-responsive">
+              <table class="table">
+                <tr>
+                  <th style="width:50%">Subtotal:</th>
+                  <td>$250.30</td>
                 </tr>
-                <tr style="box-sizing: border-box;">
-                  <th style="box-sizing: border-box;padding: 0;text-align: left;">Delivery Charge:</th>
-                  <td style="box-sizing: border-box;padding: 0;">P 25</td>
+                <tr>
+                  <th>Tax (9.3%)</th>
+                  <td>$10.34</td>
                 </tr>
-                <tr style="box-sizing: border-box;">
-                  <th style="box-sizing: border-box;padding: 0;text-align: left;">Total:</th>
-                  <td style="box-sizing: border-box;padding: 0;">{{ $totalAmount_final }}</td>
+                <tr>
+                  <th>Shipping:</th>
+                  <td>$5.80</td>
+                </tr>
+                <tr>
+                  <th>Total:</th>
+                  <td>$265.24</td>
                 </tr>
               </table>
             </div>
@@ -99,5 +150,8 @@
         </div><!-- /.row -->
       </section><!-- /.content -->
     </div><!-- ./wrapper -->
+
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/app.min.js" type="text/javascript"></script>
   </body>
 </html>
