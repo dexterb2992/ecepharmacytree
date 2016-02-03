6fa0d22a1270da23b79442192f4457917bd6d408
Zem <lourdrivera123@gmail.com>
2016-02-04 07:09:03 +0800

    seniorcitizen migration and model

---
+++ app/Http/Controllers/SeniorCitizenController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ECEPharmacyTree\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use ECEPharmacyTree\Http\Requests;
+use ECEPharmacyTree\Http\Controllers\Controller;
+
+class SeniorCitizenController extends Controller
+{
+   
+}
---
+++ app/SeniorCitizen.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ECEPharmacyTree;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SeniorCitizen extends Model
+{
+    protected $table = "senior_citizen";
+}
